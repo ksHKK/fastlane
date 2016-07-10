b622311101499a0832cc710fc50b9b8b03c6888e
ksHKK <kskeyh@gmail.com>
2016-07-11 07:57:07 +0900

    Revert "Fixed misspell."

--- fastlane/lib/fastlane/actions/update_project_provisioning.rb
+++ fastlane/lib/fastlane/actions/update_project_provisioning.rb
@@ -74,7 +74,7 @@ module Fastlane
       end
 
       def self.description
-        "Update projects code signing settings from your provisioning profile"
+        "Update projects code signing settings from your profisioning profile"
       end
 
       def self.details
